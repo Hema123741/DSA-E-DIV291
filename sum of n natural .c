7bc0624d6731bbe659983e05614ac76e9107a4e9
Hema123741 <120093652+Hema123741@users.noreply.github.com>
2024-03-11 22:21:53 +0530

    Add files via upload

---
+++ sum of n natural .c
@@ -0,0 +1,15 @@
+// modular c program to compute sum of n natural numbers
+/*
+Name:Apoorva
+Division:S
+Roll no.:1910
+Date:6/2/2023
+*/
+#include<stdio.h>
+int sum_of_n(int n);
+int main()
+{
+    int n,sum;
+    printf("Enter the positive number that how much to be added\n");
+    scanf("%d")
+}
